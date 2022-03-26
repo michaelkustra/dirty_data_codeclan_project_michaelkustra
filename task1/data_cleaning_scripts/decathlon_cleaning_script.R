11c7e9db9ed1e5d018a1a16dce4d4fa8c7afd0f9
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-03-26 10:22:53 +0000

    Clean and tidy data
    done as much as I can see for now

--- task1/data_cleaning_scripts/decathlon_cleaning_script.R
+++ task1/data_cleaning_scripts/decathlon_cleaning_script.R
@@ -16,7 +16,6 @@ raw_decathlon_data <- raw_decathlon_data %>%
 raw_decathlon_data <- tibble::rownames_to_column(raw_decathlon_data, "row_names") # Apply rownames_to_column
 raw_decathlon_data
 
-raw_decathlon_data <- rename(raw_decathlon_data, athlete_name = row_names)
 
 raw_decathlon_data
 
@@ -26,6 +25,8 @@ glimpse(raw_decathlon_data)
 head(raw_decathlon_data)
 dim(raw_decathlon_data)
 
+raw_decathlon_data <- rename(raw_decathlon_data, athlete_name = row_names)
+
 decathlon_data_long <- raw_decathlon_data %>%
   pivot_longer(cols = 2:11, 
                names_to = "event", 
@@ -34,4 +35,11 @@ decathlon_data_long <- raw_decathlon_data %>%
 glimpse(decathlon_data_long)
 dim(decathlon_data_long)
 head(decathlon_data_long)
+names(decathlon_data_long)
 has_rownames(decathlon_data_long)
+
+decathlon_data_long <- rename(decathlon_data_long, final_position = rank)
+decathlon_data_long <- rename(decathlon_data_long, ind_event_score = score)
+
+names(decathlon_data_long)
+head(decathlon_data_long)
