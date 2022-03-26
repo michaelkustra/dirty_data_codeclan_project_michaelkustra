31f80443a901be87105e1d5426a9c2c68a6db6aa
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-03-26 09:59:03 +0000

    athlete names as own column

--- task1/data_cleaning_scripts/decathlon_cleaning_script.R
+++ task1/data_cleaning_scripts/decathlon_cleaning_script.R
@@ -6,5 +6,34 @@ raw_decathlon_data <- read_rds(here("raw_data/decathlon.rds"))
 head(raw_decathlon_data)
 dim(raw_decathlon_data)
 names(raw_decathlon_data)
+raw_decathlon_data <- rowid_to_column(raw_decathlon_data) %>% head()
+names(raw_decathlon_data)
+
 raw_decathlon_data <- raw_decathlon_data %>% 
   clean_names()
+
+                                           # Duplicate example data
+raw_decathlon_data <- tibble::rownames_to_column(raw_decathlon_data, "row_names") # Apply rownames_to_column
+raw_decathlon_data
+
+raw_decathlon_data <- rename(raw_decathlon_data, athlete_name = row_names)
+
+raw_decathlon_data
+
+
+
+
+
+
+
+
+
+names(raw_decathlon_data)
+decathlon_data_long <- raw_decathlon_data %>%
+  pivot_longer(cols = 2:11, 
+               names_to = "event", 
+               values_to = "score")
+glimpse(decathlon_data_long)
+dim(decathlon_data_long)
+head(decathlon_data_long)
+has_rownames(raw_decathlon_data)
