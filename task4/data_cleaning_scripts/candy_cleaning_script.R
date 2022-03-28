2054eba8ef8739b29e4c2ef8ae0eadbfe785f5df
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-03-28 10:30:42 +0100

    combined data
    combined three data frames together using bind_rows

--- task4/data_cleaning_scripts/candy_cleaning_script.R
+++ task4/data_cleaning_scripts/candy_cleaning_script.R
@@ -1,7 +1,23 @@
 library(readxl)
+library(tidyverse)
+
 candy_2015_data <- read_xlsx("raw_data/boing-boing-candy-2015.xlsx")
 candy_2016_data <- read_xlsx("raw_data/boing-boing-candy-2016.xlsx")
 candy_2017_data <- read_xlsx("raw_data/boing-boing-candy-2017.xlsx")
+
 dim(candy_2015_data)
 dim(candy_2016_data)
 dim(candy_2017_data)
+
+names(candy_2015_data)
+names(candy_2016_data)
+names(candy_2017_data)
+
+library(janitor)
+candy_2015_data <- clean_names(candy_2015_data)
+candy_2016_data <- clean_names(candy_2016_data)
+candy_2017_data <- clean_names(candy_2017_data)
+
+combined_candy_data <- bind_rows(candy_2015_data, candy_2016_data, 
+                                 candy_2017_data)
+dim(combined_candy_data)
