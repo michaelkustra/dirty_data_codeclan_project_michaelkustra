bc3fc1fa0c010a73f1834a1fd44c297e6c181d9c
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-03-29 10:22:01 +0100

    Update on '15, '16, '17
    changed approach to clean individual years

--- task4/data_cleaning_scripts/candy_cleaning_script.R
+++ task4/data_cleaning_scripts/candy_cleaning_script.R
@@ -2,28 +2,69 @@ library(readxl)
 library(tidyverse)
 library(here)
 
+
+#clean 2015 data only
 candy_2015_data <- read_xlsx("raw_data/boing-boing-candy-2015.xlsx")
-candy_2016_data <- read_xlsx("raw_data/boing-boing-candy-2016.xlsx")
-candy_2017_data <- read_xlsx("raw_data/boing-boing-candy-2017.xlsx")
-
-dim(candy_2015_data)
-dim(candy_2016_data)
-dim(candy_2017_data)
-
-names(candy_2015_data)
-names(candy_2016_data)
-names(candy_2017_data)
-
 library(janitor)
 candy_2015_data <- clean_names(candy_2015_data)
+names(candy_2015_data)
+
+#remove unneccesary columns
+
+remove_cols_candy_2015 <- candy_2015_data[-c(1, 18, 23, 26, 27, 28, 30, 34, 37,
+                                             41, 59, 88:90, 97:99, 
+                                             100:114, 116:124)]
+
+#rename columns for ease of analysis later
+
+remove_cols_candy_2015 <- rename(remove_cols_candy_2015, age = how_old_are_you)
+
+# create year column
+remove_cols_candy_2015 <- remove_cols_candy_2015 %>% 
+  mutate(year = 2015, .before = 1)
+
+
+#clean 2016 data only
+candy_2016_data <- read_xlsx("raw_data/boing-boing-candy-2016.xlsx")
+
+#clean column names
 candy_2016_data <- clean_names(candy_2016_data)
+
+#remove unnecessary columns from data
+remove_cols_candy_2016 <- candy_2016_data[-c(1, 6, 15, 21, 22, 27, 31, 38, 43,
+                                             78:79, 102, 104:105, 107:123)]
+
+#rename columns
+remove_cols_candy_2016 <- rename(remove_cols_candy_2016, age = how_old_are_you)
+
+#create year columns
+remove_cols_candy_2016 <- remove_cols_candy_2016 %>% 
+  mutate(year = 2016, .before = 1)
+
+
+#clean 2017 data only
+candy_2017_data <- read_xlsx("raw_data/boing-boing-candy-2017.xlsx")
+
+#clean column names
 candy_2017_data <- clean_names(candy_2017_data)
 
-combined_candy_data <- bind_rows(candy_2015_data, candy_2016_data, 
-                                 candy_2017_data)
+#remove unnecessary columns
+remove_cols_candy_2017 <- candy_2017_data[-c(1, 6, 15, 21:22, 27, 31:32, 38,
+                                             43, 49, 79, 81, 105, 107:108,
+                                             110:120)]
 
-combined_candy_data %>% 
-write_csv(here("clean_data/combined_candy_data.csv"))
+#rename columns
 
+
+
+
+
+
+#combine three data sets
+#combined_candy_data <- bind_rows(candy_2015_data, candy_2016_data, 
+#                                 candy_2017_data)
+
+#combined_candy_data %>% 
+#write_csv(here("clean_data/combined_candy_data.csv"))
 
 
